b326a9d197ae1a69f119bfb61dd8b15cb87c817d
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-08-10 12:23:45 -0700

    server todo id

--- README.md
+++ README.md
@@ -5,9 +5,15 @@
 
 //What is Middlewear? How does it get used? How do I check it?
     Middlewear is the glue to all routes. It gets used on all of our routes. To check we can console log it and log it.
-    
+
 //How do I add custom middlewear to specific routes.
 
 //Why do we move code and export it and require it?
 
 module.exports = middlewear and we do this bc...
+
+//What is requiring a module and then How do I mount it?
+`npm install body-parser` and then I requred it at the top of my server.js file. And then I mounted it. When I mounted it, I did this... 
+The reason I did that was because 
+
+//Created 1st GET /todos & tested it with POSTMAN by creating Collection->Environmet->Route
--- server.js
+++ server.js
@@ -2,15 +2,58 @@ var express = require('express')
 
 var app = express() //creating an express app
 var path = require('path');
+var bodyParser = require('body-parser');
+
 var PORT = process.env.PORT || 3000;
 var middlewear = require('./middlewear')
+
+var todos = [
+  {
+    id: 1,
+    description: 'Teach REST API',
+    completed: false
+  },
+  {
+    id: 2,
+    description: 'Go eat a healthy lunch.',
+    completed: true
+  }
+]
+
 
 
 app.use(middlewear.requireAuthentication); // This MiddleWear is for the whole app!
 
+app.use(bodyParser()); //Why did we do this and what does this mean?
+
 app.get('/', function(req, res){
   res.send('<h1>Express Todo API </h1>')
 })
+
+app.get('/todos', function(req, res){
+  res.json(todos);
+})
+
+
+app.get('/todos/:id', function(req, res){ //creating a variable that will hold id from params object.
+  var todoId = parseInt(req.params.id);
+  var matchedTodo;
+  todos.forEach(function(todo){
+    if(todoId === todo.id){
+      matchedTodo = todo;
+    }
+  })
+    if(matchedTodo){
+      res.json(matchedTodo)
+    } else{
+      res.status(404).send();
+    }
+})
+
+
+
+
+
 
 app.get('/about', middlewear.logger, function(req, res){
   res.send('<h1> Express About Page</h1>')
